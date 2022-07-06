32ecdad6ca4b3588f90d1c7f37cdb57b4886965c
Ambuj-K <43566170+Ambuj-K@users.noreply.github.com>
2022-07-06 13:19:18 +0530

    Update run.js

--- blockchain_code/scripts/run.js
+++ blockchain_code/scripts/run.js
@@ -1,16 +1,31 @@
 const main = async() => {
+    
+    // initialise a few accts to send waves from locally
+    
     const [owner, randomUser, randomUser1] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+
+    // add ether for local testing
+    
     const waveContract = await waveContractFactory.deploy({
         value: hre.ethers.utils.parseEther("0.1"),
     });
+    
+    // wait for deployment on local hardhat dev env
+    
     await waveContract.deployed();
 
+    // log contract address and owner acct of hardhat
+    
     console.log("WaveFactory Deployed to ", waveContract.address);
     console.log("WaveFactory deployed by ", owner.address);
-
+    
+    // log totalwaves 
+    
     let waveCount;
     waveCount = await waveContract.displayTotalWaves();
+    
+    // log contract balance before/after each wave test
 
     let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log(
@@ -18,13 +33,19 @@ const main = async() => {
         hre.ethers.utils.formatEther(contractBalance)
     );
     
+    // send wave
+    
     let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait();
 
+    // log total waves
+    
     waveCount = await waveContract.displayTotalWaves();
 
     waveTxn = await waveContract.connect(randomUser).wave("A message!");
     await waveTxn.wait();
+    
+    // log contract balance before/after each wave test
 
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log(
@@ -34,6 +55,8 @@ const main = async() => {
 
     waveTxn = await waveContract.connect(randomUser1).wave("Another message!");
     await waveTxn.wait();
+    
+    // log contract balance before/after each wave test
 
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log(
@@ -43,6 +66,8 @@ const main = async() => {
 
     waveTxn = await waveContract.connect(randomUser).wave("A message!");
     await waveTxn.wait();
+    
+    // log contract balance before/after each wave test
 
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log(
