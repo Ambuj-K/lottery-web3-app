042aba6f95afd63341ac2ded1f3dc3bad746b05a
Ambuj-K <43566170+Ambuj-K@users.noreply.github.com>
2022-07-06 13:04:05 +0530

    Update WavePortal.sol

--- blockchain_code/contracts/WavePortal.sol
+++ blockchain_code/contracts/WavePortal.sol
@@ -10,24 +10,30 @@ contract WavePortal{
     mapping (address=>uint256) wavingUserCounts;
     uint256 timestamp;
     
-    //Event type to emit for each account that waves at us
+    // Event type to emit for each account that waves at us
     
     event NewWave(address indexed from, uint256 timestamp, string message);
     
-    //Struct type to store details of the wave sent
+    // Struct type to store details of the wave sent
 
     struct Wave {
         address waver; // The address of the user who waved.
         string message; // The message the user sent.
         uint256 timestamp; // The timestamp when the user waved.
     }
-
+    
+    //Array to store all Wave Struct event data sent 
+    
     Wave[] waves;
 
     constructor() payable{
         console.log("Constructor for the Wave and surprise wins app");
     }
-
+    
+    // wave function to add a total wave counter, emit event,
+    // and maintain a per user wave counter
+    // also send a minimum ether value for each wave
+    
     function wave(string memory _message) public{
         console.log("adding wave from ",msg.sender);
         totalWaves+=1;
@@ -42,11 +48,15 @@ contract WavePortal{
         (bool success, ) = (msg.sender).call{value: prizeAmount}("");
         require(success, "Failed to withdraw money from contract.");    
     }
-
+    
+    // returns the waves list of details of each wave
+    
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
     }
-
+    
+    // returns total waves sent to us
+    
     function displayTotalWaves() public view returns (uint256){
         console.log("total waves counter ", totalWaves);
         return totalWaves;
